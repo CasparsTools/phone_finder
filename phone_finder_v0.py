41883db9d99caec6327a35c121d58b1d4b7e79cc
Tominous <18189490+Tominous@users.noreply.github.com>
2021-12-26 21:49:12 +0000

    Update phone_finder_v0.py

--- phone_finder_v0.py
+++ phone_finder_v0.py
@@ -6,27 +6,27 @@ Phone Finder V0
 @author: augustofadel
 """
 
-#from os import listdir
+from os import listdir
 from os.path import join
 import numpy as np
 import pandas as pd
-#from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 
 from tensorflow.python import keras
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
 from tensorflow.python.keras.preprocessing.image import load_img, img_to_array
 
-#from tensorflow.contrib.keras.python import keras
-#from tensorflow.contrib.keras.python.keras.models import Sequential
-#from tensorflow.contrib.keras.python.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
-#from tensorflow.contrib.keras.python.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.contrib.keras.python import keras
+from tensorflow.contrib.keras.python.keras.models import Sequential
+from tensorflow.contrib.keras.python.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
+from tensorflow.contrib.keras.python.keras.preprocessing.image import load_img, img_to_array
 
 
 
-#image_path = 'D:/Users/augusto.fadel/Documents/!filesync/mestrado/18.1_aprendizado de maquina/trabalho2/find_phone_data'
-#image_path = '/media/augusto/AUGUSTO/mestrado/18.1_aprendizado de maquina/trabalho2/find_phone_data'
-#image_path = '/home/augusto/Desktop/filesync/mestrado/18.1_aprendizado de maquina/trabalho2/find_phone_data'
+image_path = 'D:/Users/augusto.fadel/Documents/!filesync/mestrado/18.1_aprendizado de maquina/trabalho2/find_phone_data'
+image_path = '/media/augusto/AUGUSTO/mestrado/18.1_aprendizado de maquina/trabalho2/find_phone_data'
+image_path = '/home/augusto/Desktop/filesync/mestrado/18.1_aprendizado de maquina/trabalho2/find_phone_data'
 image_path = '/home/augustofadel/phone_finder/find_phone_data'
 
 phone_location = pd.read_csv(join(image_path, 'labels.txt'),
@@ -40,10 +40,10 @@ images = [load_img(join(image_path, img),
                    target_size = (image_height, image_width)) 
           for img in phone_location['image_name']]
 
-#i = 10
-#from IPython.display import display
-#print(display(images[i]))
-#print(phone_location.loc[[i]])
+i = 10
+from IPython.display import display
+print(display(images[i]))
+print(phone_location.loc[[i]])
 
 X = np.asarray([np.array(img_to_array(img)) for img in images])/255
 y = np.asarray(phone_location.drop('image_name', axis = 1))
